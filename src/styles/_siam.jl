dfa34109f21fa1c6faca0a3bd32ee3c24f7c4038
Simon Praetorius <simon.praetorius@tu-dresden.de>
2024-08-05 01:05:34 +0200

    Continue the work on the siam bst file implementation

--- src/styles/_siam.jl
+++ src/styles/_siam.jl
@@ -1,7 +1,7 @@
 
 struct Siam <: BibliographyStyle end
 
-module BibliographyStyleSiam 
+module BibliographyStyleSiam
 
 import BibInternal
 
@@ -12,6 +12,25 @@ fieldOrNull(field)::String = empty(field) ? "" : field
 emphasize(fmt::OutputFormat, str::AbstractString) = empty(str) ? "" : outputEmph(fmt, str)
 scapify(fmt::OutputFormat, str::AbstractString) = empty(str) ? "" : outputSmallCaps(fmt, str)
 dashify(fmt::OutputFormat, str::AbstractString)::String = empty(str) ? "" : outputNumberRange(fmt,split(str,'-'))
+tieConnect(fmt::OutputFormat, arr::AbstractVector{T})::String where T = outputJoinSpace(fmt,arr)
+tieOrSpaceConnect(fmt::OutputFormat, arr::AbstractVector{T})::String where T = length(arr[end]) > 3 ? tieConnect(fmt,arr) : join(arr," ")
+
+function outputCheck!(arr::AbstractVector{T}, str::AbstractString, msg::AbstractString)
+  if empty(str)
+    @warn msg
+  else
+    push!(arr, str)
+  end
+end
+
+function output!(arr::AbstractVector{T}, str::AbstractString)
+  !empty(str) && push!(arr, str)
+end
+
+function outputNotNull!(arr::AbstractVector{T}, str::AbstractString)
+  @assert !empty(str)
+  push!(arr, str)
+end
 
 
 function formatNames(fmt::OutputFormat, names::BibInternal.Names)::String
@@ -26,7 +45,7 @@ function formatNames(fmt::OutputFormat, names::BibInternal.Names)::String
         if numnames > 2
           out *= ","
           if t == "others"
-            out *= " " * outputJoinSpace(fmt, ["et","al."]) # et~al.
+            out *= " " * tieConnect(fmt, ["et","al."]) # et~al.
           else
             out *= " and " * t
           end
@@ -39,116 +58,207 @@ function formatNames(fmt::OutputFormat, names::BibInternal.Names)::String
   out
 end
 
+"Format author names in small caps"
 function formatAuthors(fmt::OutputFormat, names::BibInternal.Names)::String
   empty(names) ? "" : scapify(fmt,formatNames(fmt,names))
 end
 
+"Format organization in small caps"
 function formatOrganization(fmt::OutputFormat, org::AbstractString)::String
   scapify(fmt,org)
 end
 
-function formatEditors(fmt::OutputFormat, names::BibInternal.Names)::String 
+"Format editor names in small caps, postfixed by 'ed(s).'"
+function formatEditors(fmt::OutputFormat, names::BibInternal.Names)::String
   empty(names) ? "" : scapify(fmt,formatNames(fmt,names)) * (length(names) > 1 ? ", eds." : ", ed.")
 end
 
-function formatIneditors(fmt::OutputFormat, names::BibInternal.Names)::String 
+"Form editor names with postfixed by 'ed(s).'"
+function formatIneditors(fmt::OutputFormat, names::BibInternal.Names)::String
   empty(names) ? "" : formatNames(fmt,names) * (length(names) > 1 ? ", eds." : ", ed.")
 end
 
+"Emphasize the title and convert it into sentence-case."
 function formatTitle(fmt::OutputFormat, title::AbstractString)::String
-  empty(title) ? "" : emphasize(fmt, uppercasefirst(title))
+  empty(title) ? "" : emphasize(fmt, uppercasefirst(lowercase(title)))
 end
 
+"Emphasize the title."
 function formatBTitle(fmt::OutputFormat, title::AbstractString)::String
   emphasize(fmt, title)
 end
 
-function formatDate(fmt::OutputFormat, date::BibInternal.Date)::String
-  if empty(date.year)
-    if empty(date.month)
+"Format the dat as '[mm ]yyyy'."
+function formatDate(fmt::OutputFormat, data::BibInternal.Entry)::String
+  if empty(data.date.year)
+    if empty(data.date.month)
       return ""
     else
-      @warn "There's a month but not year"
+      @warn "There's a month but not year in $(data.id)"
     end
   else
-    return empty(date.month) ? year : month * " " * year
+    return empty(data.date.month) ? data.date.year : data.date.month * " " * data.date.year
   end
 end
 
-function formatBVolume(fmt::OutputFormat, data::BibInternal.In)::String
-  if empty(data.volume) 
+"Format 'vol.~volume of series'."
+function formatBVolume(fmt::OutputFormat, data::BibInternal.Entry)::String
+  if empty(data.in.volume)
     return ""
   else
-    out = outputJoinSpace(fmt,["vol.",data.volume])
-    if !empty(data.series)
-      out *= " of " * series
+    out = tieConnect(fmt,["vol.",data.in.volume])
+    if !empty(data.in.series)
+      out *= " of " * data.in.series
     end
-    if !empty(data.number)
-      @warn "Can't use both volume and number"
+    if !empty(data.in.number)
+      @warn "Can't use both volume and number in $(data.id)"
     end
     return out
   end
 end
 
-function formatNumberSeries(fmt::OutputFormat, data::BibInternal.In)::String
-  if !empty(data.volume)
+"Format 'no.~number in series'."
+function formatNumberSeries(fmt::OutputFormat, data::BibInternal.Entry)::String
+  if !empty(data.in.volume)
     return "" # Can't use both volume and number
   else
-    if empty(data.number)
-      return data.series
+    if empty(data.in.number)
+      return data.in.series
     else
-      out = outputJoinSpace(fmt, ["no.",data.number])
-      if empty(data.series)
-        @warn "There's a number but no series"
+      out = tieConnect(fmt, ["no.",data.in.number])
+      if empty(data.in.series)
+        @warn "There's a number but no series in $(data.id)"
       else
-        out *= " in " * data.series
+        out *= " in " * data.in.series
       end
       return out
     end
   end
 end
 
-function formatEdition(fmt::OutputFormat, edition::AbstractString)::String
-  empty(edition) ? "" : outputJoinSpace(fmt, [lowercase(edition),"ed."])
+"Format edition postfixed by 'ed.'."
+function formatEdition(fmt::OutputFormat, data::BibInternal.Entry)::String
+  empty(data.in.edition) ? "" : tieConnect(fmt, [lowercase(data.in.edition),"ed."])
 end
 
-function formatPages(fmt::OutputFormat, pages::AbstractString)::String
-  empty(pages) ? 
-    "" : 
-    length(split(pages,r"[-,]")) > 1 ? 
-      outputJoinSpace(fmt,["pp.",dashify(fmt,pages)]) : 
-      outputJoinSpace(fmt,["p.",pages])
+"Format pages as 'pp.~1--2' or 'p.~1."
+function formatPages(fmt::OutputFormat, data::BibInternal.Entry)::String
+  empty(data.in.pages) ?
+    "" :
+    length(split(data.in.pages,r"[-,]")) > 1 ?
+      tieConnect(fmt,["pp.",dashify(fmt,data.in.pages)]) :
+      tieConnect(fmt,["p.",data.in.pages])
 end
 
-function FormatVolYear(fmt::OutputFormat, volume::AbstractString, year::AbstractString)::String
-  out = volume
+"Format as 'volume (year)'."
+function formatVolYear(fmt::OutputFormat, data::BibInternal.Entry)::String
+  out = data.in.volume
   if empty(year)
-    @warn "Empty year"
+    @warn "Empty year in $(data.id)."
   else
-    out *= " ($year)"
+    out *= " ($(data.date.year))"
   end
   out
 end
 
-formatAuthor(fmt::OutputFormat, style::Siam, von, last, junior, first, second)::String = formatAuthorFLast(fmt, von, last, junior, first, second)
+function formatChapterPages(fmt::OutputFormat, data::BibInternal.Entry)::String
+  if empty(data.in.chapter)
+    return formatPages(fmt,data)
+  else
+    out = ""
+    if empty(data.type)
+      out = tieConnect(fmt,["ch.",chapter])
+    else
+      out = tieOrSpaceConnect(fmt,[lowercase(data.in.chapter),chapter])
+      if !empty(data.in.pages)
+        out *= ", " * formatPages(fmt,data)
+      end
+    end
+    return out
+  end
+end
 
+"Formate booktitle as 'in booktitle[, editors...]'."
+function formatInEdBooktitle(fmt::OutputFormat, data::BibInternal.Entry)::String
+  if empty(data.booktitle)
+    return ""
+  else
+    if empty(data.editors)
+      return "in " * data.booktitle
+    else
+      return "in " * data.booktitle * ", " * formatInEditors(fmt,data)
+    end
+  end
+end
 
+"Check if all relevant fields for the misc type are empty."
+function emptyMiscCheck(data::BibInternal.Entry)::Bool
+  if empty(data.authors) && empty(data.title) && empty(data.access.howpublished) &&
+     empty(data.date.month) && empty(data.date.year) && haskey(data.fields,"note")
+     @warn "All relevant fields are empty in $(data.id)."
+    return false
+  else
+    return true
+  end
+end
 
-formatAuthor(fmt::OutputFormat, style::Siam, von, last, junior, first, second)::String = formatAuthorFLast(fmt, von, last, junior, first, second)
+"Transform the thesis type into lowercase if set."
+function formatThesisType(fmt::OutputFormat, data::BibInternal.Entry)::String
+  empty(data.type) ? "" : lowercase(data.type)
+end
 
-formatVolume(fmt::OutputFormat, style::Siam, volume::AbstractString) = outputJoinSpace(fmt,["vol.",volume])
-formatNumber(fmt::OutputFormat, style::Siam, volume::AbstractString) = outputJoinSpace(fmt,["no.",volume])
-formatPages(fmt::OutputFormat, style::Siam, pages::AbstractString) = !isempty(pages) ? outputJoinSpace(fmt,["pp.",outputNumberRange(fmt,split(pages,'-'))]) : ""
-formatEdition(fmt::OutputFormat, style::Siam, edition::AbstractString) = !isempty(edition) ? outputJoinSpace(fmt,[lowercase(edition),"ed."]) : ""
+"Format the tech report number as 'Tech. Rep.~number' or 'type~number'"
+function formatTrNumber(fmt::OutputFormat, data::BibInternal.Entry)::String
+  out = empty(data.type) ? "Tech. Rep." : data.type
+  empty(data.in.number) ? lowercase(out) : tieOrSpaceConnect(out,data.in.number)
+end
 
-formatEditors(style::Siam, editors::AbstractString)::String = joinNotEmpty(editors,", ",isMultipleAuthors(editors) ? "eds." : "ed.")
+# TODO: crossref not implemented
 
-formatChapter(style::Siam, chapter::AbstractString, type::AbstractString)::String = joinNotEmpty(isempty(type) ? "chapter" : lowercase(type), " ", chapter)
+function article(fmt::OutputFormat, data::BibInternal.Entry)::String
+  blocks = []
 
+  output!(blocks, let list = []
+    outputCheck!(list, formatAuthors(fmt,data.authors), "Empty 'author' in $(data.id).")
+    outputCheck!(list, formatTitle(fmt,data.title), "Empty 'title' in $(data.id).")
+    outputCheck!(list, journal, "Empty 'journal' in $(data.id).")
+    output!(list, formatVolYear(fmt,data))
+    output!(list, formatPages(fmt,data))
 
+    join(list, ", ")
+  end)
 
+  output!(blocks, get(data.fields,"note",""))
+  blocks
+end
 
+function book(fmt::OutputFormat, data::BibInternal.Entry)::String
+  blocks = []
 
+  output!(blocks, let list = []
+    if empty(data.authors)
+      outputCheck!(list, formatEditors(fmt,data.editors), "Empty 'author' and 'editor' in $(data.id).")
+    else
+      push!(list, formatAuthors(fmt,data.authors))
+      if !empty(data.editors)
+        @warn "Can't use both 'author' and 'editor' fields in $(data.id)"
+      end
+    end
+
+    outputCheck!(list, formatBTitle(fmt, data.title), "Empty 'title' in $(data.id).")
+    output!(list, formatBVolume(fmt,data))
+    output!(list, formatNumberSeries(fmt,data))
+    outputCheck!(list, data.in.publisher, "Empty 'publisher' in $(data.in).")
+    output!(list, data.in.address)
+    output!(list, formatEdition(fmt,data))
+    outputCheck!(list, formatDate(fmt,data), "Empty 'year' in $(data.in).")
+
+    join(list, ", ")
+  end)
+
+  output!(blocks, get(data.fields,"note",""))
+  blocks
+end
 
 end # module BibliographyStyleStyleSiam
 
@@ -156,75 +266,75 @@ end # module BibliographyStyleStyleSiam
 
 # F.~S. von Last, Junior, F.~M. Last2, and F.~M. Last3, ``Title,'' {\em Journal}, vol.~v123, pp.~1--2, mm yyyy.
 # This is a note.
-function formatArticle(fmt::OutputFormat, style::Siam, authors, title, journal, year; volume="", number="", pages="", month="", note="")
-  BibliographyStyleSiam.article(fmt, authors, title, journal, year, volume, number, pages, month, note)
+function formatArticle(fmt::OutputFormat, style::Siam, data::BibInternal.Entry)
+  BibliographyStyleSiam.article(fmt, data)
 end
 
 # F.~M. Last1, F.~M. Last2, and F.~M. Last3, {\em Title}, vol.~v123 of {\em Series}.
 # Address: Publisher, edition~ed., mm yyyy.
 # This is a note.
-function formatBook(fmt::OutputFormat, style::Siam, title, publisher, year; authors="", editors="", volume="", number="", series="", address="", edition="", month="", note="")
-  BibliographyStyleSiam.book(fmt, title, publisher, year, authors, editors, volume, number, series, address, edition, month, note)
+function formatBook(fmt::OutputFormat, style::Siam, data::BibInternal.Entry)
+  BibliographyStyleSiam.book(fmt, data)
 end
 
 # F.~M. Last1, F.~M. Last2, and F.~M. Last3, ``Title.'' How it is published, Address, mm yyyy.
 # This is a note.
-function formatBooklet(fmt::OutputFormat, style::Siam, title; authors="", howpublished="", address="", month="", year="", note="")
-  BibliographyStyleSiam.booklet(fmt, title, authors, howpublished, address, month, year, note)
+function formatBooklet(fmt::OutputFormat, style::Siam, data::BibInternal.Entry)
+  BibliographyStyleSiam.booklet(fmt, data)
 end
 
 # F.~M. Last1, F.~M. Last2, and F.~M. Last3, {\em This is a title}, vol.~v123 of {\em Series}, type Chapter, pp.~1--2.
 # Address: Publisher, edition~ed., mm yyyy.
 # This is a note.
-function formatInbook(fmt::OutputFormat, style::Siam, title, chapter, publisher, year; authors="", editors="", volume="", number="", series="", type="", address="", edition="", month="", pages="", note="")
-  BibliographyStyleSiam.inbook(fmt, title, chapter, publisher, year, authors, editors, volume, number, series, type, address, edition, month, pages, note)
+function formatInbook(fmt::OutputFormat, data::BibInternal.Entry)
+  BibliographyStyleSiam.inbook(fmt, data)
 end
 
 # F.~M. Last1, F.~M. Last2, and F.~M. Last3, ``Title,'' in {\em Booktitle} (E.~E. ELast1, E.~E. ELast2, and E.~E. ELast3, eds.), vol.~v123 of {\em Series}, type Chapter, pp.~1--2, Address: Publisher, edition~ed., mm yyyy.
 # This is a note.
-function formatInCollection(fmt::OutputFormat, style::Siam, authors, title, booktitle, publisher, year; editors="", volume="", number="", series="", type="", chapter="", pages="", address="", edition="", month="", note="")
-  BibliographyStyleSiam.incollection(fmt, authors, title, booktitle, publisher, year, editors, volume, number, series, type, chapter, pages, address, edition, month, note)
+function formatInCollection(fmt::OutputFormat, style::Siam, data::BibInternal.Entry)
+  BibliographyStyleSiam.incollection(fmt, data)
 end
 
 # F.~M. Last1, F.~M. Last2, and F.~M. Last3, {\em Title}.
 # Organization, Address, edition~ed., mm yyyy.
 # This is a note.
-function formatManual(fmt::OutputFormat, style::Siam, title, year; authors="", organization="", address="", edition="", month="", note="")
-  BibliographyStyleSiam.manual(fmt, title, year, authors, organization, address, edition, month, note)
+function formatManual(fmt::OutputFormat, style::Siam, data::BibInternal.Entry)
+  BibliographyStyleSiam.manual(fmt, data)
 end
 
 # F.~M. Last, ``Title,'' type, School, Address, mm yyyy.
 # This is a note.
-function formatMastersThesis(fmt::OutputFormat, style::Siam, author, title, school, year; type="", address="", month="", note="")
-  BibliographyStyleSiam.mastersthesis(fmt, author, title, school, year, type, address, month, note)
+function formatMastersThesis(fmt::OutputFormat, style::Siam, data::BibInternal.Entry)
+  BibliographyStyleSiam.mastersthesis(fmt, data)
 end
 
 # F.~M. Last1, F.~M. Last2, and F.~M. Last3, ``Title.'' How it is published, mm yyyy.
 # This is a note.
-function formatMisc(fmt::OutputFormat, style::Siam; authors="", title="", howpublished="", month="", year="", note="")
-  BibliographyStyleSiam.misc(fmt, authors, title, howpublished, month, year, note)
+function formatMisc(fmt::OutputFormat, style::Siam; data::BibInternal.Entry)
+  BibliographyStyleSiam.misc(fmt, data)
 end
 
 # F.~M. Last, {\em Title}.
 # Type, School, Address, mm yyyy.
 # This is a note.
-function formatPhDThesis(fmt::OutputFormat, style::Siam, author, title, school, year; type="", address="", month="", note="")
-  BibliographyStyleSiam.phdthesis(fmt, author, title, school, year, type, address, month, note)
+function formatPhDThesis(fmt::OutputFormat, style::Siam, data::BibInternal.Entry)
+  BibliographyStyleSiam.phdthesis(fmt, data)
 end
 
 # E.~E. ELast1, E.~E. ELast2, and E.~E. ELast3, eds., {\em Title}, vol.~v123 of {\em Series}, (Address), Organization, Publisher, mm yyyy.
 # This is a note.
-function formatProceedings(fmt::OutputFormat, style::Siam, title, year; editors="", volume="", number="", series="", organization="", address="", month="", publisher="", note="")
-  BibliographyStyleSiam.proceedings(fmt, title, year, editors, volume, number, series, organization, address, month, publisher, note)
+function formatProceedings(fmt::OutputFormat, style::Siam, data::BibInternal.Entry)
+  BibliographyStyleSiam.proceedings(fmt, data)
 end
 
 # F.~M. Last1, F.~M. Last2, and F.~M. Last3, ``Title,'' Type n234, Institution, Address, mm yyyy.
 # This is a note.
-function formatTechreport(fmt::OutputFormat, style::Siam, authors, title, institution, year; type="", number="", address="", month="", note="")
-  BibliographyStyleSiam.techreport(fmt, authors, title, institution, year, type, number, address, month, note)
+function formatTechreport(fmt::OutputFormat, style::Siam, data::BibInternal.Entry)
+  BibliographyStyleSiam.techreport(fmt, data)
 end
 
 # F.~M. Last1, F.~M. Last2, and F.~M. Last3, ``Title.'' This is a note, mm yyyy.
-function formatUnpublished(fmt::OutputFormat, style::Siam, authors, title, note; howpublished="", month="", year="")
-  BibliographyStyleSiam.pubpublished(fmt, authors, title, note, howpublished, month, year)
+function formatUnpublished(fmt::OutputFormat, style::Siam, data::BibInternal.Entry)
+  BibliographyStyleSiam.pubpublished(fmt, data)
 end
