3a6a394b65fbf280849dbdaa7c2050bf0dde8ebb
Simon Praetorius <simon.praetorius@tu-dresden.de>
2024-08-05 01:05:21 +0200

    Change nowrap space in html output

--- src/formats/html.jl
+++ src/formats/html.jl
@@ -3,5 +3,6 @@ struct OutputFormatHtml <: OutputFormat end
 outputEmph(fmt::OutputFormatHtml, str::AbstractString) = "<em>$str</em>"
 outputSmallCaps(fmt::OutputFormatHtml, str::AbstractString) = "<span class=\"sc\">$str</span>"
 outputQuote(fmt::OutputFormatHtml, str::AbstractString) = "&ldquo;$str&rdquo;"
+outputJoinSpace(fmt::OutputFormatLatex, list::AbstractVector{S}) where S = join(list, "&nbsp;")
 outputNumberRange(fmt::OutputFormatHtml, pair::AbstractVector{S}) where {S<:AbstractString} = join(pair, "&ndash;")
 outputBlocks(fmt::OutputFormatHtml, blocks::AbstractVector{S}) where S = join(blocks, "\n")
